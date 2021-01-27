73057416b1310bb189e29cf4c146efc60a1beb58
Manako <akirk15@gmail.com>
2021-01-26 20:24:52 -0500

    First commit
    Adding basic project

---
+++ LostandFound/Assets/GamePlay/Scripts/playerScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+
+{
+    public CharacterController pControls;
+    public float speed = 12f;
+
+    Vector3 velocity;
+    bool isGrounded;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        pControls.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        pControls.Move(velocity * Time.deltaTime);
+    }
+}
+
