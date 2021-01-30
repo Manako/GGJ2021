cb1deaeef81f47608d5e563299e4cce0bdfae962
Manako <akirk15@gmail.com>
2021-01-30 09:20:27 -0500

    got starting text functioning

--- LostandFound/Assets/GamePlay/Scripts/MouseHandler.cs
+++ LostandFound/Assets/GamePlay/Scripts/MouseHandler.cs
@@ -5,24 +5,28 @@ public class MouseHandler : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
     public Transform playerBody;
+    public bool lockCursor = false;
 
     float xRotation = 0f;
 
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        if (lockCursor)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
 
 
     }
